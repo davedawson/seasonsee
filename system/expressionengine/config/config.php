082b75bc8e8c9fba193943fed03cafac1dbc4332
davedawson <dave@iamdavedawson.com>
2014-01-02 20:35:07 -0500

    set config to base

--- system/expressionengine/config/config.php
+++ system/expressionengine/config/config.php
@@ -327,7 +327,7 @@ $config['proxy_ips'] = "";
 /**
  * Require the Focus Lab, LLC Master Config file
  */
-require $_SERVER['DOCUMENT_ROOT'] . '/home/102864/domains/dev.whatsinseasontoday.com/html/config/config.master.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/config/config.master.php';
 
 
 /* End of file config.php */
