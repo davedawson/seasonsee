6dbbf5b122931fae5b88f46b8b2d57582345d3d8
davedawson <dave@iamdavedawson.com>
2014-01-02 19:34:20 -0500

    make config unignored

---
+++ config/config.prod.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Production config overrides & db credentials
+ *
+ * Our database credentials and any environment-specific overrides
+ *
+ * @package    Focus Lab Master Config
+ * @version    1.1.1
+ * @author     Focus Lab, LLC <dev@focuslabllc.com>
+ */
+
+$env_db['hostname'] = 'localhost';
+$env_db['username'] = 'db102864_admin';
+$env_db['password'] = 'd9d14d88';
+$env_db['database'] = 'db102864_seasonsee';
+
+// Sample global variable for Production only
+// Can be used in templates like "{global:google_analytics}"
+$env_global['global:google_analytics'] = 'UA-XXXXXXX-XX';
+
+/* End of file config.prod.php */
+/* Location: ./config/config.prod.php */
